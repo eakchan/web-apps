07f7b591cfb7930e4c5513ea8f4902ef582b4d44
eakchan <53607081+eakchan@users.noreply.github.com>
2020-07-09 22:36:24 +0530

    initial bolg web page

---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './style.css';
+
+/**
+* @author
+* @function Header
+**/
+
+const Header = (props) => {
+  return(
+    <header className="header">
+        <nav className="headerMenu">
+            <a href="{C:\Users\akash\Desktop\eakchan\HTML\React\blog\src\containers\Home\index.js}">Home</a>
+            <a href="#">About Us</a>
+            <a href="#">Contact Us</a>
+        </nav>
+        <div>
+            socila Media links
+        </div>
+    </header>
+   )
+
+ }
+
+export default Header
